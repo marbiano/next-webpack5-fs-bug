19a981bd26e8d62e5efadb55c5a9162cf6df1879
Martin Bavio <mbavio@gmail.com>
2021-05-21 23:54:34 -0300

    Initial setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import getContent from "../../lib/test";
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const content = getContent();
+  res.json(content);
+};
