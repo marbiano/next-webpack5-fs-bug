5e3fa791518bde7e8c56d83b32ed3e39e2d69bde
Martin Bavio <mbavio@gmail.com>
2021-08-26 16:58:56 -0300

    Add Sentry basic setup

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,9 @@
+import { withSentry } from "@sentry/nextjs";
 import getContent from "../../lib/test";
 
-export default async (req, res) => {
+const handler = async (req, res) => {
   const content = getContent();
   res.json(content);
 };
+
+export default withSentry(handler);
--- pages/api/hello2.js
+++ pages/api/hello2.js
@@ -1,6 +1,10 @@
+import { withSentry } from "@sentry/nextjs";
 import getContent from "../../lib/test";
 
-export default async (req, res) => {
+const handler = async (req, res) => {
+  throw new Error("API throw error test");
   const content = getContent();
   res.json(content);
 };
+
+export default withSentry(handler);
